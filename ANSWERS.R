1c560d69b79a8df6852a7c1dcebf91d569889403
lonken <lonken@nd.edu>
2019-11-13 15:07:10 -0500

    Finished Problem 1

---
+++ ANSWERS.R
@@ -0,0 +1,21 @@
+# Clear working environment
+
+rm(list = ls())
+
+# PROBLEM NUMBER 1
+
+# READ iris.csv into the variable iris
+iris <- read.csv("iris.csv")
+
+# This is the oddrows function. It uses seq to make a vector of odd numbers to the amount of rows.
+oddrows <- function(x){
+  odds <- x[seq(from=1, to=nrow(x),by=2),]
+  
+  return(odds)
+}
+
+# This runs the new function on iris
+oddrows(iris)
+
+
+# PROBLEM NUMBER 2
